8d2b38e3126106e62b3a4871cf25405203c061ef
Vishal <noobvishal007@gmail.com>
2019-06-18 20:19:34 +0530

    Update sample_config.py

--- haruka/sample_config.py
+++ haruka/sample_config.py
@@ -35,7 +35,7 @@ class Config(object):
     BAN_STICKER = 'CAADBQADDQADjR\_yKCDDBtuwTt59Ag'  # banhammer marie sticker
     STRICT_GBAN = True
     STRICT_GMUTE = True
-    ALLOW_EXCL = True  # Allow ! commands as well as /
+    ALLOW_EXCL = False  # Allow ! commands as well as /
     API_OPENWEATHER = '' # OpenWeather API
 
     # MEMES
