edb0da5124f461d9fa4e8eee887d0e3404873e9e
Vishal <noobvishal007@gmail.com>
2019-06-18 20:32:08 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -135,7 +135,7 @@ def send_start(bot, update):
 
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
     text = "*Heya {} ;), My name is Sunny!*\n\n\
-    I'm a group manager bot maintained by @noobvishal.
+    I'm a group manager bot maintained by @noobvishal."
     
     text += "My Souce Available Here [Source](https://github.com/noobvishal/MissSunny)\n\n\
     Want to me to handle you? [Click here!](t.me/MissSunny_Bot?startgroup=true)\n\n\
