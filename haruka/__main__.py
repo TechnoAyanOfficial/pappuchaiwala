c2f64c1a37c101793f118ef7c27cc2d491ef45e8
noobanon <51384033+noobanon@users.noreply.github.com>
2019-09-28 19:47:58 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -141,7 +141,7 @@ I'm a Group Manager Bot maintained by [This NooB](t.me/anonyCrew) \n\n"
 **Support Group** Added\n\n\
 Special Thanks to peaktogoo."
 
-    keyboard = [[InlineKeyboardButton(text="🛠 Support Group", url='"htps://t.me/zonerschat")]]
+    keyboard = [[InlineKeyboardButton(text="📣 Support Group", url="https://t.me/zonerschat")]]
     keyboard = [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
